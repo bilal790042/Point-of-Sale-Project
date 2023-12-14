5eb13f111d21c97c28e05c8ab97f8d9c4d7848c9
Bilal Ahmad <151175746+bilal790042@users.noreply.github.com>
2023-12-14 15:04:59 +0500

    Uploaded file via my computer directory
    i have created Django project and also i did create my first page which is the StartUp page of my project

---
+++ StartUp_Page/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StartupPageConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'StartUp_Page'
---
+++ StartUp_Page/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def display(request):
+    return render(request, "StartUp_Page/startPage.html");
