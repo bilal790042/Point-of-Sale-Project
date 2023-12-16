77c59a7c974047928842712ee33b68dba439b4f1
Bilal Ahmad <151175746+bilal790042@users.noreply.github.com>
2023-12-16 23:56:54 +0500

    File added from Computer after changes
    i added sign in page and sign up pages and now i will do more project

--- StartUp_Page/urls.py
+++ StartUp_Page/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "StartUp_Page"
 urlpatterns = [
-    path("", views.display, name = "display page")
+    path("", views.display, name = "display_page"),
+    path("signIn", views.signIn, name = "signIn")
 ]
--- StartUp_Page/views.py
+++ StartUp_Page/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def display(request):
     return render(request, "StartUp_Page/startPage.html");
+
+
+def signIn(request):
+    return render(request, "StartUp_Page/SigninPage.html");
